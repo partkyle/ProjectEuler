3b7c42ca7f3683d374b1550cc9403f01eeb4c083
partkyle <partkyle@gmail.com>
2011-03-07 22:22:10 -0800

    initial commit

---
+++ src/com/partkyle/euler/Solution.java
@@ -0,0 +1,5 @@
+package com.partkyle.euler;
+
+public interface Solution {
+	public void solve();
+}
---
+++ src/com/partkyle/euler/solutions/Problem1.java
@@ -0,0 +1,16 @@
+package com.partkyle.euler.solutions;
+
+import com.partkyle.euler.Solution;
+
+public class Problem1 implements Solution {
+	@Override
+	public void solve() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		Solution solution = new Problem1();
+		solution.solve();
+	}
+}
